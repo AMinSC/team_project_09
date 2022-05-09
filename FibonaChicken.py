6ae94ae2d297ac3c7b16118a856b54f4c5157cc1
AMinSC <https://github.com/AMinSC/fastcampus_study.git>
2022-05-09 21:13:54 +0900

    feat: FibonaChicken code up

--- FibonaChicken.py
+++ FibonaChicken.py
@@ -1,4 +1,12 @@
-print("https://codingdojang.com/scode/596?langby=python#answer-filter-area")
-print("태성이는 위 코드에서 맥주 추가")
-print("성수는 위 코드에서 샐러드 추가")
-print("화이팅!")
+def chicken(x):
+    if x == 0: return 0
+    if x == 1: return 1
+    a,b = 1,1
+    while 1:
+        c = a+b
+        if c > x: return a + chicken(x-b)
+        elif c == x: return b
+        a, b = b, c
+
+num = int(input('자연수: '))
+print('황금비: {}'.format(chicken(num)))
