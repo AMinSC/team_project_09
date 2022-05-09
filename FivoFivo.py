5eedcc9e9b78c09bf4800807a1ab50e450afef1a
ByuNDark <urbishrleef@gmail.com>
2022-05-09 20:54:40 +0900

    feat: print 맥주 추가

--- FivoFivo.py
+++ FivoFivo.py
@@ -1,5 +1,12 @@
-print("https://codingdojang.com/scode/596?langby=python#answer-filter-area")
-print("태성이는 위 코드에서 맥주 추가")
-print("성수는 위 코드에서 샐러드 추가")
-print("화이팅!")
-print("힘들다")
+def chicken(x):
+    if x == 0: return 0
+    if x == 1: return 1
+    a,b = 1,1
+    while 1:
+        c = a+b
+        if c > x: return a + chicken(x-b)
+        elif c == x: return b
+        a, b = b, c
+
+num = int(input('사람 수: '))
+print('맥주: {}ml'.format(chicken(num) * 500))
